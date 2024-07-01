0b971b6ec1ff6da5d36a6c90aca84d8318c50320
Deepa Gurung <deepa.gurung@infodevelopers.local>
2024-06-30 23:38:55 -0700

    updated stock

--- InventoryManagementSystem/Models/CustomerDetails.cs
+++ InventoryManagementSystem/Models/CustomerDetails.cs
@@ -7,9 +7,18 @@ namespace InventoryManagementSystem.Models
         public int CustomerID { get; set; }
 
         [StringLength(20, ErrorMessage = "The field must be a string with a maximum length of 20.")]
+        [Required(ErrorMessage = "CustomerName field is required.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "CustomerName can only contain letters.")]
         public string CustomerName { get; set; }
-        public string Address { get; set; } 
+
+        [StringLength(20, ErrorMessage = "The field must be a string with a maximum length of 20.")]
+        [Required(ErrorMessage = "Address field is required.")]
+        public string Address { get; set; }
+        [Required(ErrorMessage = "Email field is required.")]
         public string Email { get; set; }
+
+        [StringLength(10, ErrorMessage = "The field must be a string with a maximum length of 10.")]
+        [Required(ErrorMessage = "Contact field is required.")]
         public string Phone { get; set; }
     }
 }
--- InventoryManagementSystem/obj/Debug/net8.0/InventoryManagementSystem.AssemblyInfo.cs
+++ InventoryManagementSystem/obj/Debug/net8.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd56ac8d147c38207641c6b5c8aa8644f0836273")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f457ce5e886abd9ba43644652c92f9aa5634f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
